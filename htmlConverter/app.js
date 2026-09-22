0aabbb8755e3ab3847d6818d1c7cce9224a9daa6
Kit Jasper M. Caballero <kcaballero_250000000510@uic.edu.ph>
2026-09-22 21:17:22 +0800

    Function markdown and console.log testings

--- htmlConverter/app.js
+++ htmlConverter/app.js
@@ -15,6 +15,17 @@ function parseMarkdown(markdown){
     html = html.replace(/^#####(.*$)/gim, '<h1>$1</h1>');
     html = html.replace(/^###### (.*$)/gim, '<h1>$1</h1>');
 
+    // Bold
+    html = html.replace(/\*\*(.*$)/gim, '<strong>$1</strong>');
+
+    // Italic
+    html = html.replace(/\*\*(.*?)/gim, '<em>$1</em>');
+
+    // Links
+    html = html.replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2" target="_blank">$1</a>')
+
+    // Line break
+    html = html.replace(/\n/gim, '<br />')
 }
 
 // Event listeners
@@ -26,4 +37,8 @@ submitBtn.addEventListener('click', (event) => {
 
     rawOutput.value = convertedHTML;
     htmlPreview.innerHTML = convertedHTML;
+
+    console.log("Markdown Conversion");
+    console.log(`Original Input: ${inputText}`);
+    console.log(`Converted Raw: ${convertedHTML}`);
 });

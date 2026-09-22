6862e722b80065def08774412fedc10a18fc9e2a
Kit Jasper M. Caballero <kcaballero_250000000510@uic.edu.ph>
2026-09-22 21:04:30 +0800

    Comments and event refinement

--- htmlConverter/app.js
+++ htmlConverter/app.js
@@ -1,15 +1,21 @@
+// Elements
 const markdownInput = document.getElementById('markdown-input');
 const submitBtn = document.getElementById('submit-btn');
 const rawOutput = document.getElementById('raw-output');
 const htmlPreview = document.getElementById('html-preview');
 
+// Function
 function parseMarkdown(markdown){
 
 }
 
+// Event listeners
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
     const inputText = markdownInput.ariaValueMax;
     const convertedHTML = parseMarkdown(inputText);
+
+    rawOutput.value = convertedHTML;
+    htmlPreview.innerHTML = convertedHTML;
 });
